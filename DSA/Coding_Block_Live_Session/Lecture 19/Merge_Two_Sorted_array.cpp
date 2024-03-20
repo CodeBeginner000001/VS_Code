99b47bf5a358e69a303c1d0c8c6d4198b3eff14b
Adarsh <ashu2100ag@gmail.com>
2024-03-20 23:45:04 +0530

    studing lecture 19

---
+++ DSA/Coding_Block_Live_Session/Lecture 19/Merge_Two_Sorted_array.cpp
@@ -0,0 +1,86 @@
+/*
+Merge Sorted Arrays
+
+Given two sorted array consisting of N and M integers respectively,
+design an algorithm to merge the two sorted array such that the
+merged array is sorted.
+
+Example
+
+Input:
+
+0  1  2  3  4
+10 30 50 70 80
+
+Output:
+
+0  1  2
+20 40 60
+
+*/
+#include <iostream>
+using namespace std;
+
+void merge_two_sorted(int arr1[], int arr2[], int n, int m)
+{
+    int k = 0;
+    int i = 0;
+    int j = 0;
+    int arr3[n + m];
+    while (i <= n - 1 && j <= m - 1)
+    {
+        if (arr1[i] < arr2[j])
+        {
+            arr3[k] = arr1[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            arr3[k] = arr2[j];
+            j++;
+            k++;
+        }
+    }
+
+    while (i <= n - 1)
+    {
+        arr3[k] = arr1[i];
+        k++;
+        i++;
+    }
+
+    while (j <= m - 1)
+    {
+        arr3[k] = arr2[j];
+        k++;
+        j++;
+    }
+
+    for (int i = 0; i <= n + m - 1; i++)
+    {
+        cout << arr3[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr1[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr1[i];
+    }
+
+    int m;
+    cin >> m;
+    int arr2[m];
+    for (int i = 0; i < m; i++)
+    {
+        cin >> arr2[i];
+    }
+
+    merge_two_sorted(arr1, arr2, n, m);
+}
---
+++ DSA/Coding_Block_Live_Session/Lecture 19/Merge_Two_Sorted_array_with_space.cpp
@@ -0,0 +1,76 @@
+/*
+Merge Sorted Arrays in O(1) Space
+
+Given a sorted array  "A" of size N+M consisting of N integers
+& another sorted array "B" consisting of M integers, design an
+algorithm to merge the two sorted array such that the merged
+array is also sorted without using any extra space.
+
+Example
+
+Input:
+0  1  2  3  4  5  6
+10 30 50 70
+
+0  1  2
+20 40 60
+
+Output:
+0  1  2  3  4  5  6
+10 20 30 40 50 60 70
+
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int arr[n + m];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int arr2[m];
+    for (int i = 0; i < m; i++)
+    {
+        cin >> arr2[i];
+    }
+
+    int i = n - 1;
+    int j = m - 1;
+    int k = m + n - 1;
+    while (i >= 0 && j >= 0)
+    {
+        if (arr[i] > arr2[j])
+        {
+            arr[k] = arr[i];
+            k--;
+            i--;
+        }
+        else
+        {
+            arr[k] = arr2[j];
+            k--;
+            j--;
+        }
+    }
+    while (i >= 0)
+    {
+        arr[k] = arr[i];
+        k--;
+        i--;
+    }
+    while (j >= 0)
+    {
+        arr[k] = arr2[j];
+        k--;
+        j--;
+    }
+
+    for (int i = 0; i < n + m; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
