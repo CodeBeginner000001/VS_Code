383abee159ca89c03a3353783ce72e7737f567a2
Adarsh <ashu2100ag@gmail.com>
2024-08-01 00:05:23 +0530

    2nd

---
+++ Array/Easy/4_Median_Two_SortedArray.cpp
@@ -0,0 +1,81 @@
+/**
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)).
+
+
+
+Example 1:
+
+Input: nums1 = [1,3], nums2 = [2]
+Output: 2.00000
+Explanation: merged array = [1,2,3] and median is 2.
+Example 2:
+
+Input: nums1 = [1,2], nums2 = [3,4]
+Output: 2.50000
+Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+
+Constraints:
+
+nums1.length == m
+nums2.length == n
+0 <= m <= 1000
+0 <= n <= 1000
+1 <= m + n <= 2000
+-106 <= nums1[i], nums2[i] <= 106
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
+    int arr1[n + m];
+    memset(arr1, 0, sizeof(arr1));
+    int arr2[m];
+    for (int i = 0; i < n; i++)
+        cin >> arr1[i];
+    for (int i = 0; i < m; i++)
+        cin >> arr2[i];
+    //  Merging Sorted Array
+    int i = n - 1;
+    int j = m - 1;
+    int k = n + m - 1;
+    while (i >= 0 && j >= 0)
+    {
+        if (arr1[i] > arr2[j])
+        {
+            arr1[k] = arr1[i];
+            i--;
+        }
+        else
+        {
+            arr1[k] = arr2[j];
+            j--;
+        }
+        k--;
+    }
+    while (j >= 0)
+    {
+        arr1[k] = arr2[j];
+        j--;
+        k--;
+    }
+    // Finding Median
+    if ((m + n) % 2 == 0)
+    {
+        int mid1 = (m + n) / 2;
+        float num = (arr1[mid1 - 1] + arr1[mid1]) / 2.0;
+        cout << num << endl;
+    }
+    else
+    {
+        int odd_mid = ceil((n + m) / 2);
+        float num = arr1[odd_mid] / 1.0;
+        cout << num << endl;
+    }
+}
--- Array/Easy/88_Merge_Sorted_Array.cpp
+++ Array/Easy/88_Merge_Sorted_Array.cpp
@@ -1,11 +1,11 @@
 /**
-You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, 
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
 representing the number of elements in nums1 and nums2 respectively.
 
 Merge nums1 and nums2 into a single array sorted in non-decreasing order.
 
-The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
-To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
 and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
 Example 1:
@@ -27,7 +27,7 @@ Output: [1]
 Explanation: The arrays we are merging are [] and [1].
 The result of the merge is [1].
 Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
- 
+
 
 Constraints:
 
@@ -36,9 +36,47 @@ nums2.length == n
 0 <= m, n <= 200
 1 <= m + n <= 200
 -109 <= nums1[i], nums2[j] <= 109
- 
+
 
 Follow up: Can you come up with an algorithm that runs in O(m + n) time?
 */
 
-#include<iostream>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int m;
+    cin >> m;
+    int arr1[m + n];
+    memset(arr1, 0, sizeof(arr1));
+    for (int i = 0; i < n; i++)
+        cin >> arr1[i];
+    int arr2[m];
+    for (int i = 0; i < m; i++)
+        cin >> arr2[i];
+        int i = n-1;
+        int j= m-1;
+        int k = m+n-1;
+        while (i >= 0 && j >= 0)
+        {
+            if(arr1[i]>arr2[j])
+            {
+                arr1[k]=arr1[i];
+                i--;
+            }
+            else{
+                arr1[k]=arr2[j];
+                j--;
+            }
+            k--;
+        }
+        while(j>=0)
+        {
+            arr1[k]=arr2[j];
+            j--;
+            k--;
+        }
+      for(int i=0;i<(n+m);i++) cout<<arr1[i]<<" ";
+}
