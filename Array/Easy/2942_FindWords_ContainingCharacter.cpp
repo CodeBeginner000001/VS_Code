3cff92672ae3c867c6d5abb0bf3ec83f2d5d3b02
Adarsh <ashu2100ag@gmail.com>
2024-08-01 22:24:35 +0530

    3rd Day

---
+++ Array/Easy/2942_FindWords_ContainingCharacter.cpp
@@ -0,0 +1,56 @@
+/**
+You are given a 0-indexed array of strings words and a character x.
+
+Return an array of indices representing the words that contain the character x.
+
+Note that the returned array may be in any order.
+
+ 
+
+Example 1:
+
+Input: words = ["leet","code"], x = "e"
+Output: [0,1]
+Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+Example 2:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+Output: [0,2]
+Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+Example 3:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+Output: []
+Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+ 
+
+Constraints:
+
+1 <= words.length <= 50
+1 <= words[i].length <= 50
+x is a lowercase English letter.
+words[i] consists only of lowercase English letters.
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    string arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    char x;
+    cin>>x;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<arr[i].length();j++)
+        {
+            if(arr[i][j]==x) 
+            {
+                cout<<i<<" ";
+                break;
+            }
+        }
+    }
+    cout<<endl;
+}
