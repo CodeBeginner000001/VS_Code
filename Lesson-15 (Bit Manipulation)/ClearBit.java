59ef6d26727cd34390981148a731170c987f565c
Adarsh <ashu2100ag@gmail.com>
2024-10-25 00:34:10 +0530

    Completed Lesson-15

---
+++ Lesson-15 (Bit Manipulation)/ClearBit.java
@@ -0,0 +1,11 @@
+public class ClearBit {
+    public static void main(String args[])
+    {
+        int n=5; //0101
+        int pos=2;
+        int bitmask = 1<<pos; // 0100
+        int notbitmask = ~bitmask; //1011
+        int number = notbitmask & n; //001
+        System.out.println(number); // 1
+    }
+}
